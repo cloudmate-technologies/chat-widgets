aa9e4227c350b7201d7a35fd0b8659867e73330b
Sanjay Katariya <sanjay.k@cloudmatetechnologies.com>
2025-05-30 15:30:36 +0530

    remove code

--- dist/components/ChatWrapper.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const ChatWrapper: () => import("solid-js").JSX.Element;
-//# sourceMappingURL=ChatWrapper.d.ts.map
--- dist/components/product/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './ProductCard';
-export * from './ProductGrid';
-//# sourceMappingURL=index.d.ts.map
