9b3d16765e7543363a2183555a30af78da756d15
Sanjay Katariya <sanjay.k@cloudmatetechnologies.com>
2025-06-13 16:46:19 +0530

    update latest dist

--- src/web.ts
+++ src/web.ts
@@ -4,7 +4,6 @@ import { parseChatbot, injectChatbotInWindow } from './window';
 registerWebComponents();
 
 const chatbot = parseChatbot();
-console.log('chatbot', chatbot);
 injectChatbotInWindow(chatbot);
 
 export default chatbot;
